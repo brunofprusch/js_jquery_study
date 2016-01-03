6b8c25043a507b14ab963d0307334b8f9722237a
brunofprusch <brunoprusch@gmail.com>
2016-01-03 20:43:51 -0200

    Implementando o método para atualização de valor.

--- js/core.js
+++ js/core.js
@@ -13,3 +13,33 @@ function readTotal() {
 	var total = document.getElementById("total");
 	return moneyTextToFloat(total.innerHTML);
 }
+
+function writeTotal(value) {
+	var total = document.getElementById("total");
+	total.innerHTML = floatToMoneyText(value);
+}
+
+function calculationTotal() {
+
+	var produtos = document.getElementsByClassName("product");
+
+	var total = parseFloat("0");
+
+	for(var i = 0; i < produtos.length; i++) {
+		var priceElement = produtos[i].getElementsByClassName("price");
+		var priceText = priceElement[0].innerHTML;
+		var price = moneyTextToFloat(priceText);
+
+		console.log(price);
+
+		var quantityElement = produtos[i].getElementsByClassName("quantity");
+		var quantityText = quantityElement[0].value;
+		var quantity = moneyTextToFloat(quantityText);
+
+		console.log(quantity);
+
+		total = total + (quantity * price);
+	}
+
+	writeTotal(total);
+}
